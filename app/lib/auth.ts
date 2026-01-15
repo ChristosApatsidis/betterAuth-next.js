d3f3bbc31d9fa09012d0cd9a4f32041862e586af
ChristosApatsidis <christos.apatsidis@icloud.com>
2026-01-15 14:02:31 +0200

    Add BETTER_AUTH_URL to trustedOrigins for better authentication configuration

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -14,7 +14,8 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL as string,
   trustedOrigins: [
     "http://localhost:3000",
-    "http://*:3000"
+    "http://*:3000",
+    process.env.BETTER_AUTH_URL as string,
   ],
   rateLimit: {
     enabled: true,
